5c35421b443066b477905e6b716c0763b1ad24c3
pepeciavirella <pep.ciavirella@gmail.com>
2018-06-27 19:16:24 -0300

    Subido el ejercicio 1
    Signed-off-by: pepeciavirella <pep.ciavirella@gmail.com>

---
+++ Ejercicio01/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;                                              //Para recorrer el array
+    int enteros[10];
+    for(i = 0; i <= 9; i++)
+    {
+        cout << "Ingrese el entero numero " << i+1 << ": ";
+        cin >> enteros[i];
+    }
+    cout << endl << "--FIN DEL INGRESO--" << endl << endl;
+    cout << "El orden inverso en el cual se ingresaron fue:" << endl;
+    for(i = 9; i >= 0; i--)                             //Reutilizo el mismo contador, le puedo asignar 9 al arrancar el for
+    {
+        cout << enteros[i] << endl;
+    }
+    return 0;
+}
